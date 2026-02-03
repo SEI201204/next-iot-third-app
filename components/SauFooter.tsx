bc4da8ad534af2a8e8b11c697053f3bdf958e18c
SEI201204 <s6519410036@sau.ac.th>
2026-02-04 00:14:18 +0700

    Ver1.0

---
+++ components/SauFooter.tsx
@@ -0,0 +1,11 @@
+export function SauFooter() {
+  return (
+    <div className="mt-10 mb-20" >
+      <h3 className="text-center text-medium">
+        Southeast Asia University
+        <br />
+        Copyright © 2024 by Thitiporn
+      </h3>
+    </div>
+  );
+}
